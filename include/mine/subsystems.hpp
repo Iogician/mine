b921eabf1c1de78ada84f3d05c63af346142c090
Jaden Hernandez <hernandezjaden4@gmail.com>
2023-05-12 16:25:53 -0700

    Reverse intake rotation

--- include/mine/subsystems.hpp
+++ include/mine/subsystems.hpp
@@ -3,8 +3,8 @@
 using namespace pros;
 
 namespace intake {
-    #define INTAKE_PORT1 6
-    #define INTAKE_PORT2 9
+    #define INTAKE_PORT1 -6
+    #define INTAKE_PORT2 -9
 
     extern MotorGroup motors;
     void run(double percent = 100, int time = 0);
